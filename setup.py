37906fcb7c54f3085f57dede1395986bff0ce45b
Alexey Shagaleev <alexey.shagaleev@yandex.ru>
2018-10-22 12:14:31 +0500

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dispatcher',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     url='https://github.com/ShagaleevAlexey/dispatcher',
     license='',
